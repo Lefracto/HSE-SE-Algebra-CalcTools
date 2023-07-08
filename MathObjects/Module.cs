0d4a3f4ebb54aab924cdeaa483b47ce9090430eb
Anton Pauliuchyk <tauifyn@mail.ru>
2023-07-08 22:38:45 +0300

    Module class

---
+++ MathObjects/Module.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MathObjects
+{
+    public class Module
+    {
+        private readonly int _value;
+        private readonly int _module;
+
+        public Module()
+        {
+            _module = 0;
+            _value = 0;
+        }
+        public Module(int value, int module)
+        {
+            if (module <= 0)
+            {
+                //exception
+            }
+            _module = module;
+            while (value < 0)
+            {
+                value += module;
+            }
+            _value = value % _module;
+        }
+        
+        public Module GetInverseByAddition()
+        {
+            var inverse = _module - _value;
+            while (inverse < 0)
+            {
+                inverse += _module;
+            }
+
+            return new Module(inverse, _module);
+        }
+        
+        public Module Add(Module module) => new Module(module._value + _value, _module);
+        
+        public Module Multiply(Module module) => new Module(module._value * _value, _module);
+        
+        public Module Minus(Module module)  => new Module(module._value - _value, _module);
+        
+        public override string ToString() => _value.ToString();
+    }
+}
--- MathObjects/Rational.cs
+++ MathObjects/Rational.cs
@@ -5,30 +5,51 @@ namespace MathObjects
     public class Rational
     {
         private int _numerator;
-        private int _denumerator;
-
+        private int _denominator;
+        public int Numerator
+        {
+            get => _numerator;
+          //  private set => _numerator = value;
+        }
+        
+        public int Denominator
+        {
+            get => _denominator;
+            //private set => _denominator = value;
+        }
+        
         public Rational()
         {
             _numerator = 0;
-            _denumerator = 1;
+            _denominator = 1;
         }
 
-        public Rational(int num, int denum)
+        public Rational(int numerator, int denominator)
         {
-            _numerator = num;
-
-            if (denum == 0)
+            // make shorter
+            
+            if (denominator == 0)
             {
                 //exception;
             }
-            
-            _denumerator = denum;
+            _numerator = numerator;
+            _denominator = denominator;
         }
         
+        public Rational Add(Rational rational) => new Rational(
+            _numerator * rational._denominator + rational._numerator * _denominator,
+            _denominator * rational._denominator);
         
+        public Rational Minus(Rational rational) => new Rational(
+            _numerator * rational._denominator - rational._numerator * _denominator,
+            _denominator * rational._denominator);
         
-        public override string ToString() => _denumerator == 1
+        public Rational Multiply(Rational rational) => new Rational(
+            _numerator * rational._numerator,
+            _denominator * rational._denominator);
+        
+        public override string ToString() => _denominator == 1
             ? _numerator.ToString()
-            : _numerator.ToString() + "/" + _denumerator.ToString();
+            : _numerator + "/" + _denominator;
     }
 }
