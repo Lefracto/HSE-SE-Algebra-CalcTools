bb3b77814150ce5939ddb863ee13a405ade52f0c
Anton Pauliuchyk <tauifyn@mail.ru>
2023-07-08 14:46:19 +0300

    Rational class

--- CalcToolsIO/Program.cs
+++ CalcToolsIO/Program.cs
@@ -7,36 +7,7 @@ namespace HSE_SE_Algebra_CalcTools
     {
         static void Main(string[] args)
         {
-            TestInt[][] v = new TestInt[2][];
-            TestInt[][] v1 = new TestInt[2][];
             
-            v[0] = new TestInt[2];
-            v[0][0] = new TestInt(3);
-            v[0][1] = new TestInt(5);
-            
-            v[1] = new TestInt[2];
-            v[1][0] = new TestInt(3);
-            v[1][1] = new TestInt(7);
-            
-            
-            
-            v1[0] = new TestInt[2];
-            v1[0][0] = new TestInt(1);
-            v1[0][1] = new TestInt(0);
-            
-            v1[1] = new TestInt[2];
-            v1[1][0] = new TestInt(0);
-            v1[1][1] = new TestInt(1);
-
-            Matrix<TestInt> matrix = new Matrix<TestInt>(v);
-            Matrix<TestInt> matrix2 = new Matrix<TestInt>(v1);
-            
-            Console.WriteLine(matrix);
-            Console.WriteLine();
-            Console.WriteLine(matrix2);
-            Console.WriteLine();
-            matrix.Transpose();
-            Console.WriteLine(matrix);
         }
     }
 }
---
+++ MathObjects/Rational.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace MathObjects
+{
+    public class Rational
+    {
+        private int _numerator;
+        private int _denumerator;
+
+        public Rational()
+        {
+            _numerator = 0;
+            _denumerator = 1;
+        }
+
+        public Rational(int num, int denum)
+        {
+            _numerator = num;
+
+            if (denum == 0)
+            {
+                //exception;
+            }
+            
+            _denumerator = denum;
+        }
+        
+        
+        
+        public override string ToString() => _denumerator == 1
+            ? _numerator.ToString()
+            : _numerator.ToString() + "/" + _denumerator.ToString();
+    }
+}
