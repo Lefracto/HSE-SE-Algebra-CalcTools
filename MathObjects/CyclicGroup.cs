2aaade88bce3962b535878f584f60d59b7dacd3a
Anton Pauliuchyk <tauifyn@mail.ru>
2023-07-15 18:38:00 +0300

    CyclicGroup class

---
+++ MathObjects/CyclicGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathObjects
+{
+    public class CyclicGroup
+    {
+        private int _order;
+        
+        public CyclicGroup(int order)
+        {
+            if (order <= 0)
+                throw new ArgumentException("Period must be >0");
+            _order = order;
+        }
+
+        public int GetOrderOfElement(int element)
+        {
+            if (element < 0 || element > _order)
+                throw new ArgumentException("This group doesn't have such element");
+
+            var elementOrder = 0;
+            var sum = element;
+            while (sum != 0)
+            {
+                sum += element;
+                elementOrder++;
+            }
+            
+            return elementOrder;
+        }
+    }
+}
